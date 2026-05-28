1db9a5d437cc8fc0a3e9d55cf7108739b0015796
Theo-L-000 <theodore.lee26@ycdsbk12.ca>
2026-05-28 14:00:42 -0400

    Draw Default Circle + Add Rarity Decider Method

--- src/Sketch.java
+++ src/Sketch.java
@@ -5,6 +5,14 @@ import processing.core.PApplet;
  * @author Theodore Lee and Jordan Lam
  */
 public class Sketch extends PApplet {
+
+    /* DECLARE GLOBAL VARIABLES */
+    int rarity = 1; /* (1 = common, 2 = uncommon, 3 = rare, 4 = super rare,
+    5 = epic, 6 = mythic, 7 = legendary, 8 = ultra) */
+    int circleX = 600;
+    int circleY = 400;
+    int circleRadius = 250;
+
     public static void main(String[] args) {
         PApplet.main("Sketch");
     }
@@ -24,7 +32,20 @@ public class Sketch extends PApplet {
     /* ANIMATION */ 
     @Override
     public void draw() {
+        decideRarity(rarity); // Draw sticker depending on rarity
+    }
 
+    public void decideRarity(int rarityIndicator) {
+        if (rarityIndicator == 1){
+            common();
+        }
+    }
+
+    public void common() {
+        background(220, 252, 249);  // Re-draw background to erase previous frame
+        fill(255, 208, 67);         // Yellow fill
+        strokeWeight(5);            // Outline thickness
+        circle(circleX, circleY, circleRadius * 2);      
     }
 
 }
