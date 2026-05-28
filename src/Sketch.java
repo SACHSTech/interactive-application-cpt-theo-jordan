ac9bfb569a154e88ef4eb990ab0215d47b03b1db
Theo-L-000 <theodore.lee26@ycdsbk12.ca>
2026-05-28 14:06:44 -0400

    Draw Uncommon + Rare Circle

--- src/Sketch.java
+++ src/Sketch.java
@@ -7,11 +7,12 @@ import processing.core.PApplet;
 public class Sketch extends PApplet {
 
     /* DECLARE GLOBAL VARIABLES */
-    int rarity = 1; /* (1 = common, 2 = uncommon, 3 = rare, 4 = super rare,
+    int rarity = 3; /* (1 = common, 2 = uncommon, 3 = rare, 4 = super rare,
     5 = epic, 6 = mythic, 7 = legendary, 8 = ultra) */
     int circleX = 600;
     int circleY = 400;
     int circleRadius = 250;
+    String[] rarityText = {"Common", "Uncommon", "Rare", "Super Rare", "Epic", "Mythic", "Legendary", "Ultra"};
 
     public static void main(String[] args) {
         PApplet.main("Sketch");
@@ -32,17 +33,35 @@ public class Sketch extends PApplet {
     /* ANIMATION */ 
     @Override
     public void draw() {
-        decideRarity(rarity); // Draw sticker depending on rarity
+        decideRarity(rarity);  // Draw sticker depending on rarity
     }
 
     public void decideRarity(int rarityIndicator) {
-        if (rarityIndicator == 1){
+        if (rarityIndicator == 1) {
             common();
+        } else if (rarityIndicator == 2) {
+            uncommon();
+        } else if (rarityIndicator == 3) {
+            rare();
         }
     }
 
     public void common() {
         background(220, 252, 249);  // Re-draw background to erase previous frame
+        fill(255, 208, 67);         // Yellow fill
+        strokeWeight(5);            // Outline thickness
+        circle(circleX, circleY, circleRadius * 2);      
+    }
+
+    public void uncommon() {
+        background(225, 255, 221);  // Re-draw background to erase previous frame
+        fill(255, 208, 67);         // Yellow fill
+        strokeWeight(5);            // Outline thickness
+        circle(circleX, circleY, circleRadius * 2);      
+    }
+
+    public void rare() {
+        background(100, 255, 79);  // Re-draw background to erase previous frame
         fill(255, 208, 67);         // Yellow fill
         strokeWeight(5);            // Outline thickness
         circle(circleX, circleY, circleRadius * 2);      
