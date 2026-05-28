836222251e1da33d383886fdf6562deb7515586b
Theo-L-000 <theodore.lee26@ycdsbk12.ca>
2026-05-28 14:21:21 -0400

    Draw Remaining Circles + Add drawSticker() Method

--- src/Sketch.java
+++ src/Sketch.java
@@ -7,7 +7,7 @@ import processing.core.PApplet;
 public class Sketch extends PApplet {
 
     /* DECLARE GLOBAL VARIABLES */
-    int rarity = 1; /* (1 = common, 2 = uncommon, 3 = rare, 4 = super rare,
+    int rarity = 4; /* (1 = common, 2 = uncommon, 3 = rare, 4 = super rare,
     5 = epic, 6 = mythic, 7 = legendary, 8 = ultra) */
     int circleX = 600;
     int circleY = 400;
@@ -34,6 +34,7 @@ public class Sketch extends PApplet {
     @Override
     public void draw() {
         decideRarity(rarity);  // Draw sticker depending on rarity
+        drawSticker();
     }
 
     public void decideRarity(int rarityIndicator) {
@@ -43,6 +44,16 @@ public class Sketch extends PApplet {
             uncommon();
         } else if (rarityIndicator == 3) {
             rare();
+        } else if (rarityIndicator == 4) {
+            superRare();
+        } else if (rarityIndicator == 5) {
+            epic();
+        } else if (rarityIndicator == 6) {
+            mythic();
+        } else if (rarityIndicator == 7) {
+            legendary();
+        } else if (rarityIndicator == 8) {
+            ultra();
         }
     }
 
@@ -54,27 +65,49 @@ public class Sketch extends PApplet {
     }
 
     public void common() {
-        background(220, 252, 249);  // Re-draw background to erase previous frame
-        fill(255, 208, 67);         // Yellow fill
-        strokeWeight(5);            // Outline thickness
-        circle(circleX, circleY, circleRadius * 2);
+        background(220, 252, 249);  // Light blue
         displayRarity(rarityText[0]);      
     }
 
     public void uncommon() {
-        background(225, 255, 221);  // Re-draw background to erase previous frame
-        fill(255, 208, 67);         // Yellow fill
-        strokeWeight(5);            // Outline thickness
-        circle(circleX, circleY, circleRadius * 2);      
+        background(225, 255, 221);  // Light green
+        displayRarity(rarityText[1]);   
     }
 
     public void rare() {
-        background(100, 255, 79);  // Re-draw background to erase previous frame
-        fill(255, 208, 67);         // Yellow fill
-        strokeWeight(5);            // Outline thickness
-        circle(circleX, circleY, circleRadius * 2);      
+        background(100, 255, 79);  // Green
+        displayRarity(rarityText[2]);   
     }
 
-    
+    public void superRare() {
+        background(0, 150, 232);  // Blue
+        displayRarity(rarityText[3]);   
+    }
+
+    public void epic() {
+        background(163, 0, 232);  // Dark purple
+        displayRarity(rarityText[4]);   
+    }
+
+    public void mythic() {
+        background(230, 0, 0);  // Red
+        displayRarity(rarityText[5]);   
+    }
+
+    public void legendary() {
+        background(252, 255, 0);  // Yellow
+        displayRarity(rarityText[6]);   
+    }
+
+    public void ultra() {
+        background(255, 0, 189);  // Light purple
+        displayRarity(rarityText[7]);   
+    }
+
+    public void drawSticker(){
+        fill(255, 208, 67);         // Yellow fill
+        strokeWeight(5);            // Outline thickness
+        circle(circleX, circleY, circleRadius * 2); 
+    }
 
 }
