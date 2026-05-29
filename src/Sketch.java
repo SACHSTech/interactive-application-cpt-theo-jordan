9d5c4c3b5e52a6299eea3e065ff423e30f270a0e
Theo-L-000 <theodore.lee26@ycdsbk12.ca>
2026-05-29 12:10:18 -0400

    Display + Increment Score When Circle Clicked

--- src/Sketch.java
+++ src/Sketch.java
@@ -7,12 +7,14 @@ import processing.core.PApplet;
 public class Sketch extends PApplet {
 
     /* DECLARE GLOBAL VARIABLES */
-    int rarity = 4; /* (1 = common, 2 = uncommon, 3 = rare, 4 = super rare,
+    int rarity = 1; /* (1 = common, 2 = uncommon, 3 = rare, 4 = super rare,
     5 = epic, 6 = mythic, 7 = legendary, 8 = ultra) */
     int circleX = 600;
     int circleY = 400;
     int circleRadius = 250;
+    int score = 0;
     String[] rarityText = {"Common", "Uncommon", "Rare", "Super Rare", "Epic", "Mythic", "Legendary", "Ultra"};
+    int[] clicks = {5, 15, 30, 50, 75, 100, 130};
 
     public static void main(String[] args) {
         PApplet.main("Sketch");
@@ -35,8 +37,11 @@ public class Sketch extends PApplet {
     public void draw() {
         decideRarity(rarity);  // Draw sticker depending on rarity
         drawSticker();
+        displayScore(score);
     }
 
+    /* 
+    */
     public void decideRarity(int rarityIndicator) {
         if (rarityIndicator == 1) {
             common();
@@ -57,11 +62,38 @@ public class Sketch extends PApplet {
         }
     }
 
+    /* 
+    */
     public void displayRarity(String rarityDisplayText) {
-        fill(0);                            // Black fill
-        textSize(100);                      // Text size
-        textAlign(CENTER);                  // Make text appear in center
-        text(rarityDisplayText, (width / 2), 110);       // Display instructions for ball control
+        fill(0);                                    // Black
+        textSize(100); 
+        textAlign(CENTER);                          // Make text appear in center
+        text(rarityDisplayText, (width / 2), 110);  // Display rarity
+    }
+
+    /* 
+    */
+    public void displayScore(int clickScore) {
+        fill(0);                                          // Black 
+        textSize(70);   
+        textAlign(LEFT);                                  // Make text appear in left
+        text("Clicks: " + clickScore, width - 1150, 95);  // Display # of clicks
+    }
+
+    /* 
+    */
+    public void mousePressed() {
+       float distanceMiddleCircle = dist(mouseX, mouseY, circleX, circleY);
+
+       if (distanceMiddleCircle < circleRadius) {
+            score++;
+       } 
+    }
+
+    public void drawSticker(){
+        fill(255, 208, 67);         // Yellow fill
+        strokeWeight(5);            // Outline thickness
+        circle(circleX, circleY, circleRadius * 2); 
     }
 
     public void common() {
@@ -102,12 +134,6 @@ public class Sketch extends PApplet {
     public void ultra() {
         background(255, 0, 189);  // Light purple
         displayRarity(rarityText[7]);   
-    }
-
-    public void drawSticker(){
-        fill(255, 208, 67);         // Yellow fill
-        strokeWeight(5);            // Outline thickness
-        circle(circleX, circleY, circleRadius * 2); 
     }
 
 }
