7bab523a2e2446c2e60e973d39639b4ace1342d8
Theo-L-000 <theodore.lee26@ycdsbk12.ca>
2026-05-29 12:52:32 -0400

    Add Comments

--- src/Sketch.java
+++ src/Sketch.java
@@ -1,24 +1,26 @@
 import processing.core.PApplet;
 
 /**
- * REPLACE ME LATER
- * @author Theodore Lee and Jordan Lam
+ * In this game, the player clicks the circle sticker in order to upgrade it to a new rarity (new background + text). 
+ * Depending on the number of clicks (score), the sticker will change to the next rarity. However, if the 
+ * player does not click the sticker and clicks the background instead, it will not count as a click. 
+ * @author Theodore Lee and Jordan Lam 
  */
 public class Sketch extends PApplet {
 
     /* DECLARE GLOBAL VARIABLES */
-    int rarity = 1; /* (1 = common, 2 = uncommon, 3 = rare, 4 = super rare,
-    5 = epic, 6 = mythic, 7 = legendary, 8 = ultra) */
+    int rarity = 1;  // (1 = common, 2 = uncommon..., 8 = ultra)
     int circleX = 600;
     int circleY = 400;
     int circleRadius = 250;
     int score = 0;
     String[] rarityText = {"Common", "Uncommon", "Rare", "Super Rare", "Epic", "Mythic", "Legendary", "Ultra"};
-    int[] clicks = {5, 15, 30, 50, 75, 100, 130};
+    int[] clicks = {0, 10, 30, 60, 100, 150, 200, 300, Integer.MAX_VALUE};  // Clicks required to upgrade rarity (Index match up to the rarity #)
 
+    /* RUNNING SKETCH */ 
     public static void main(String[] args) {
         PApplet.main("Sketch");
-    }
+    } 
 
     /* CANVAS */
     @Override
@@ -29,18 +31,20 @@ public class Sketch extends PApplet {
     /* SETUP */ 
     @Override
     public void setup() {
-        background(220, 252, 249);  // Light Cyan
+        background(220, 252, 249);  // Light cyan (common rarity)
     }
 
     /* ANIMATION */ 
     @Override
     public void draw() {
-        decideRarity(rarity);  // Draw sticker depending on rarity
+        decideRarity(rarity);  // Background and text of current rarity
         drawSticker();
         displayScore(score);
     }
 
-    /* 
+    /**  
+     * Determines which rarity method to run depending on the current rarity
+     * @param rarityIndicator the current rarity number
     */
     public void decideRarity(int rarityIndicator) {
         if (rarityIndicator == 1) {
@@ -62,75 +66,92 @@ public class Sketch extends PApplet {
         }
     }
 
-    /* 
+    /**
+     * Displays text depending on rarity
+     * @param rarityDisplayText text to display
     */
     public void displayRarity(String rarityDisplayText) {
         fill(0);                                    // Black
         textSize(100); 
-        textAlign(CENTER);                          // Make text appear in center
+        textAlign(CENTER);                          // Center text
         text(rarityDisplayText, (width / 2), 110);  // Display rarity
     }
 
-    /* 
-    */
+    /**
+     * Displays current score
+     * @param clickScore current # of clicks
+     */
     public void displayScore(int clickScore) {
-        fill(0);                                          // Black 
+        fill(0);          // Black 
         textSize(70);   
-        textAlign(LEFT);                                  // Make text appear in left
-        text("Clicks: " + clickScore, width - 1150, 95);  // Display # of clicks
+        textAlign(LEFT);  // Align text to left
+        text("Clicks: " + clickScore, width - 1150, height - 50);  // Display # of clicks
     }
 
-    /* 
-    */
+    /** 
+     * Checks if sticker is clicked, adds to score, changes rarity when certain # of clicks reached
+     */
     public void mousePressed() {
-       float distanceMiddleCircle = dist(mouseX, mouseY, circleX, circleY);
+       float distanceMiddleCircle = dist(mouseX, mouseY, circleX, circleY);  // Distance between the circle and mouse
 
-       if (distanceMiddleCircle < circleRadius) {
+       if (distanceMiddleCircle < circleRadius) {  // Checks if mouse is clicked inside sticker radius
             score++;
+            if (score >= clicks[rarity]) {  // Check if current score is large enough to upgrade to next rarity
+                rarity++;  // Changes Rarity
+            }
        } 
     }
 
-    public void drawSticker(){
-        fill(255, 208, 67);         // Yellow fill
-        strokeWeight(5);            // Outline thickness
+    // Base of Sticker
+    public void drawSticker() {
+        fill(255, 208, 67);  // Yellow fill
+        strokeWeight(5);     // Outline thickness
         circle(circleX, circleY, circleRadius * 2); 
     }
 
+    // Display Common Rarity
     public void common() {
         background(220, 252, 249);  // Light blue
         displayRarity(rarityText[0]);      
     }
 
+    // Display Uncommon Rarity
     public void uncommon() {
         background(225, 255, 221);  // Light green
         displayRarity(rarityText[1]);   
     }
 
+    // Display Rare Rarity
     public void rare() {
         background(100, 255, 79);  // Green
         displayRarity(rarityText[2]);   
     }
 
+    // Display Super Rare Rarity
     public void superRare() {
         background(0, 150, 232);  // Blue
         displayRarity(rarityText[3]);   
     }
 
+    // Display Epic Rarity
     public void epic() {
         background(163, 0, 232);  // Dark purple
         displayRarity(rarityText[4]);   
     }
 
+    // Display Mythic Rarity
     public void mythic() {
         background(230, 0, 0);  // Red
         displayRarity(rarityText[5]);   
     }
 
+    // Display Legendary Rarity
     public void legendary() {
         background(252, 255, 0);  // Yellow
         displayRarity(rarityText[6]);   
     }
 
+    // Display Ultra Rarity
     public void ultra() {
         background(255, 0, 189);  // Light purple
         displayRarity(rarityText[7]);   
